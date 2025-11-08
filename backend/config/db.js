ff8fb0020c3325e353a6442a8e1acde06b533829
jathar jay <jayjathar31@gmail.com>
2025-11-08 07:58:08 +0530

    colth

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://jayjathar31:1234jay@cluster0.rlo8w3t.mongodb.net/hoodies-website"
-    )
-    .then(() => console.log("DB Connected"));
-};
